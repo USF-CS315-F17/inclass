2085157a869de53467cee5c96bbb899da9f7c1c7
Greg Benson <benson@usfca.edu>
2017-09-05 18:29:24 +0000

    Added print_char_bits.c example.

---
+++ week03/print_char_bits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_char_bits(char c)
+{
+        return;
+}
+
+
+int main(int argc, char **argv)
+{
+        printf("char : %c\n", 'a');
+        printf("ascii: %d\n", 'a');
+        print_char_bits('a');
+}
